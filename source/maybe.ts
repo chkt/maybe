2b14cf3136a8529bc46e749a6431875c1c336cc0
ck <ck@chkt.one>
2023-12-22 02:56:40 +0000

    Add mergeMessagesAb(), mergeMessagesBa()

--- source/maybe.ts
+++ source/maybe.ts
@@ -1,21 +1,43 @@
-import { Failure, Failures, FailuresProvider } from './failure';
+import { createMessage, Message, MessageComposite, Messages } from './message';
 
 
-export interface Result<T> extends FailuresProvider {
+export interface Result<T> extends MessageComposite {
 	readonly value : T;
 }
 
-export type Maybe<T, F extends Failure = Failure> = Result<T> | F;
+/**
+ * @alias of Message
+ */
+export type Failure<T> = Message<T>;
+
+export type Maybe<T, F = unknown> = Result<T> | Failure<F>;
 
 
-export function isResult<T>(maybe:Maybe<T>) : maybe is Result<T> {
+export function isResult<T, F>(maybe:Maybe<T, F>) : maybe is Result<T> {
 	return 'value' in maybe;
 }
 
-export function isFailure<T, U>(maybe:Maybe<T, Failure<U>>) : maybe is Failure<U> {
+export function isFailure<T, F>(maybe:Maybe<T, F>) : maybe is Failure<F> {
 	return 'severity' in maybe;
 }
 
-export function createResult<T>(value:T, messages:Failures = []) : Result<T> {
+export function createResult<T>(value:T, messages:Messages = []) : Result<T> {
 	return { value, messages };
+}
+
+/**
+ * @alias of createMessage
+ */
+export const createFailure = createMessage;
+
+export function mergeMessagesAb<T extends MessageComposite>(a:T, b:Maybe<unknown>) : T {
+	const messages = isResult(b) ? b.messages : [ b ];
+
+	return { ...a, messages : [ ...a.messages, ...messages ] };
+}
+
+export function mergeMessagesBa<T extends MessageComposite>(a:T, b:Maybe<unknown>) : T {
+	const messages = isResult(b) ? b.messages : [ b ];
+
+	return { ...a, messages : [ ...messages, ...a.messages ] };
 }
--- test/maybe.ts
+++ test/maybe.ts
@@ -1,6 +1,8 @@
-import * as assert from 'assert';
+import * as assert from 'node:assert';
 import { describe, it } from 'mocha';
-import { createResult, failureSeverity, isFailure, isResult } from '../source';
+import { createFailure, createResult, isFailure, isResult, mergeMessagesAb, mergeMessagesBa } from '../source/maybe';
+import { createMessage, messageSeverity } from '../source/message';
+
 
 
 /* eslint-disable @typescript-eslint/no-magic-numbers */
@@ -10,14 +12,14 @@ describe('isResult', () => {
 		assert.strictEqual(isResult({
 			value : 1,
 			messages : [{
-				severity : failureSeverity.error,
-				message : 'foo',
+				severity : messageSeverity.error,
+				text : 'foo',
 				messages : []
 			}]
 		}), true);
 		assert.strictEqual(isResult({
-			severity : failureSeverity.error,
-			message : 'foo',
+			severity : messageSeverity.error,
+			text : 'foo',
 			messages : []
 		}), false);
 	});
@@ -29,14 +31,14 @@ describe('isFailure', () => {
 		assert.strictEqual(isFailure({
 			value : 'foo',
 			messages : [{
-				severity : failureSeverity.error,
-				message : 'bar',
+				severity : messageSeverity.error,
+				text : 'bar',
 				messages : []
 			}]
 		}), false);
 		assert.strictEqual(isFailure({
-			severity : failureSeverity.error,
-			message : 'foo',
+			severity : messageSeverity.error,
+			text : 'foo',
 			messages : []
 		}), true);
 	});
@@ -45,8 +47,8 @@ describe('isFailure', () => {
 describe('createResult', () => {
 	it('should create a Result', () => {
 		const failure = {
-			severity : failureSeverity.error,
-			message : 'bar',
+			severity : messageSeverity.error,
+			text : 'bar',
 			messages : []
 		};
 		const result = createResult('foo', [ failure ]);
@@ -58,5 +60,75 @@ describe('createResult', () => {
 		});
 
 		assert.deepStrictEqual(createResult('foo'), { value : 'foo', messages : [] });
+	});
+});
+
+describe('createFailure', () => {
+	it('should alias createMessage', () => {
+		assert.strictEqual(createFailure, createMessage);
+	});
+});
+
+describe('mergeMessagesAb', () => {
+	it('should merge the messages of Maybe A and B', () => {
+		const f0 = createFailure('f0');
+		const f1 = createFailure('f1');
+		const f2 = createFailure('f2', messageSeverity.warn, [ f1, f0 ]);
+		const r2 = createResult('r2', [ f1, f0 ]);
+		const f3 = createFailure('f3');
+		const f4 = createFailure('f4');
+		const f5 = createFailure('f5', messageSeverity.warn, [ f3, f4 ]);
+		const r5 = createResult('r5', [ f3, f4 ]);
+
+		assert.deepStrictEqual(mergeMessagesAb(f2, f5), {
+			text : 'f2',
+			severity : messageSeverity.warn,
+			messages : [ f1, f0, f5 ]
+		});
+		assert.deepStrictEqual(mergeMessagesAb(r2, f5), {
+			value : 'r2',
+			messages : [ f1, f0, f5 ]
+		});
+		assert.deepStrictEqual(mergeMessagesAb(f2, r5), {
+			text : 'f2',
+			severity : messageSeverity.warn,
+			messages : [ f1, f0, f3, f4 ]
+		});
+		assert.deepStrictEqual(mergeMessagesAb(r2, r5), {
+			value : 'r2',
+			messages : [ f1, f0, f3, f4 ]
+		});
+	});
+});
+
+describe('mergeMessagesBa', () => {
+	it('should merge the messages of Maybe B and A', () => {
+		const f0 = createFailure('f0');
+		const f1 = createFailure('f1');
+		const f2 = createFailure('f2', messageSeverity.warn, [ f1, f0 ]);
+		const r2 = createResult('r2', [ f1, f0 ]);
+		const f3 = createFailure('f3');
+		const f4 = createFailure('f4');
+		const f5 = createFailure('f5', messageSeverity.warn, [ f3, f4 ]);
+		const r5 = createResult('r5', [ f3, f4 ]);
+
+		assert.deepStrictEqual(mergeMessagesBa(f2, f5), {
+			text : 'f2',
+			severity : messageSeverity.warn,
+			messages : [ f5, f1, f0 ]
+		});
+		assert.deepStrictEqual(mergeMessagesBa(r2, f5), {
+			value : 'r2',
+			messages : [ f5, f1, f0 ]
+		});
+		assert.deepStrictEqual(mergeMessagesBa(f2, r5), {
+			text : 'f2',
+			severity : messageSeverity.warn,
+			messages : [ f3, f4, f1, f0 ]
+		});
+		assert.deepStrictEqual(mergeMessagesBa(r2, r5), {
+			value : 'r2',
+			messages : [ f3, f4, f1, f0 ]
+		});
 	});
 });
