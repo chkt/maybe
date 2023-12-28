2f77f9adf08d397bd55d1081d237abf74742ba86
ck <ck@chkt.one>
2023-12-28 19:15:05 +0000

    Update exposed interface

--- source/index.ts
+++ source/index.ts
@@ -1,4 +1,57 @@
-export * from './failure';
-export * from './maybe';
-export * from './flow';
-export * from './native';
+export {
+	MessageSeverity,
+	messageSeverity,
+	ErrorMessage,
+	CardinalMessage,
+	TextMessage,
+	DataMessage,
+	Message,
+	Messages,
+	isErrorMessage,
+	isCardinalMessage,
+	isTextMessage,
+	isDataMessage,
+	createErrorMessage,
+	createCardinalMessage,
+	createTextMessage,
+	createDataMessage,
+	createMessage,
+	resolveMessageValue,
+	containsMessage,
+	flattenMessage,
+	flattenMessages
+} from './message';
+export {
+	Result,
+	Failure,
+	Maybe,
+	isResult,
+	isFailure,
+	createResult,
+	createFailure
+} from './maybe';
+export {
+	maybeFrom,
+	maybeAsync,
+	resultFrom,
+	resultAsync,
+	failureFrom,
+	failureAsync
+} from './native';
+export {
+	all,
+	allAsync,
+	may,
+	resolve
+} from './convert';
+export {
+	and,
+	andAsync,
+	or,
+	orAsync,
+	onResult,
+	onResultAsync,
+	onFailure,
+	onFailureAsync
+} from './flow';
+export { apply } from './compose';
--- test/index.ts
+++ test/index.ts
@@ -1,87 +1,113 @@
 import * as assert from 'assert';
 import { describe, it } from 'mocha';
-import * as failure from '../source/failure';
-import * as maybe from '../source/maybe';
-import * as flow from '../source/flow';
-import * as native from '../source/native';
 import * as root from '../source';
+import * as compose from '../source/compose';
+import * as convert from '../source/convert';
+import * as flow from '../source/flow';
+import * as maybe from '../source/maybe';
+import * as message from '../source/message';
+import * as native from '../source/native';
 
 
 /* eslint-disable @typescript-eslint/no-unused-vars, @typescript-eslint/no-magic-numbers */
-describe('failure api', () => {
+describe('message api', () => {
 	it('should contain exposed interfaces', () => {
-		const a:root.FailureSeverity = 0;
-		const b:root.ErrorFailure = { severity : 0, error : new Error(), messages : [] };
-		const c:root.CardinalFailure = { severity : 0, code : 1, messages : [] };
-		const d:root.MessageFailure = { severity : 0, message : 'foo', messages : [] };
-		const e:root.DataFailure<{ foo : number }> = { severity : 0, data : { foo : 1 }, messages : [] };
-		let f:root.Failure = b;
+		const a:root.MessageSeverity = 0;
+		const b:root.ErrorMessage<Error> = { severity : 0, error : new Error(), messages : [] };
+		const c:root.CardinalMessage = { severity : 0, code : 1, messages : [] };
+		const d:root.TextMessage = { severity : 0, text : 'foo', messages : [] };
+		const e:root.DataMessage<{ foo : number }> = { severity : 0, data : { foo : 1 }, messages : [] };
+		let f:root.Message<Error | { foo : number }> = b;
 
 		f = c;
 		f = d;
 		f = e;
 
-		const g:failure.FailureSeverity = a;
-		const h:failure.ErrorFailure = b;
-		const i:failure.CardinalFailure = c;
-		const j:failure.MessageFailure = d;
-		const k:failure.DataFailure<{ foo : number }> = e;
-		const l:root.Failures = [{ severity : 0, message : 'foo', messages : [] }];
-		const m:failure.Failures = l;
+		const g:message.MessageSeverity = a;
+		const h:message.ErrorMessage<Error> = b;
+		const i:message.CardinalMessage = c;
+		const j:message.TextMessage = d;
+		const k:message.DataMessage<{ foo : number }> = e;
+		const l:root.Messages = [{ severity : 0, text : 'foo', messages : [] }];
+		const m:message.Messages = l;
 	});
 
 	it('should contain exposed enums', () => {
-		assert.strictEqual(root.failureSeverity.error, failure.failureSeverity.error);
+		assert.strictEqual(root.messageSeverity.error, message.messageSeverity.error);
 	});
 
 	it('should contain exposed methods', () => {
-		assert.strictEqual(root.isErrorFailure, failure.isErrorFailure);
-		assert.strictEqual(root.isCardinalFailure, failure.isCardinalFailure);
-		assert.strictEqual(root.isMessageFailure, failure.isMessageFailure);
-		assert.strictEqual(root.isDataFailure, failure.isDataFailure);
-		assert.strictEqual(root.createErrorFailure, failure.createErrorFailure);
-		assert.strictEqual(root.createCardinalFailure, failure.createCardinalFailure);
-		assert.strictEqual(root.createMessageFailure, failure.createMessageFailure);
-		assert.strictEqual(root.createDataFailure, failure.createDataFailure);
-		assert.strictEqual(root.createFailure, failure.createFailure);
-		assert.strictEqual(root.resolveFailureValue, failure.resolveFailureValue);
+		assert.strictEqual(root.isErrorMessage, message.isErrorMessage);
+		assert.strictEqual(root.isCardinalMessage, message.isCardinalMessage);
+		assert.strictEqual(root.isTextMessage, message.isTextMessage);
+		assert.strictEqual(root.isDataMessage, message.isDataMessage);
+		assert.strictEqual(root.createErrorMessage, message.createErrorMessage);
+		assert.strictEqual(root.createCardinalMessage, message.createCardinalMessage);
+		assert.strictEqual(root.createTextMessage, message.createTextMessage);
+		assert.strictEqual(root.createDataMessage, message.createDataMessage);
+		assert.strictEqual(root.createMessage, message.createMessage);
+		assert.strictEqual(root.resolveMessageValue, message.resolveMessageValue);
+		assert.strictEqual(root.containsMessage, message.containsMessage);
+		assert.strictEqual(root.flattenMessage, message.flattenMessage);
+		assert.strictEqual(root.flattenMessages, message.flattenMessages);
 	});
 });
 
 describe('result api', () => {
 	it('should contain exposed interfaces', () => {
-		const a:root.Result<{ foo : 1 }> = { value : { foo : 1 }, messages : [] };
-		const b:root.Maybe<{ foo : 1 }> = { value : { foo : 1 }, messages : [] };
-		const c:root.Maybe<{ foo : 1 }> = { severity : 0, message : 'foo', messages : [] };
-		const d:maybe.Result<{ foo : 1 }> = a;
-		const e:maybe.Maybe<{ foo : 1 }> = b;
-		const f:maybe.Maybe<{ foo : 1 }> = c;
+		const a:root.Failure<Error> = { error : new Error(), severity : message.messageSeverity.warn, messages : [] };
+		const b:root.Result<{ foo : 1 }> = { value : { foo : 1 }, messages : [] };
+		const c:root.Maybe<{ foo : 1 }> = { value : { foo : 1 }, messages : [] };
+		const d:root.Maybe<{ foo : 1 }> = { text : 'foo', severity : message.messageSeverity.warn, messages : [] };
+		const e:maybe.Failure<Error> = a;
+		const f:maybe.Result<{ foo : 1 }> = b;
+		const g:maybe.Maybe<{ foo : 1 }> = c;
+		const h:maybe.Maybe<{ foo : 1 }> = d;
 	});
 
 	it('should contain exposed methods', () => {
 		assert.strictEqual(root.isResult, maybe.isResult);
 		assert.strictEqual(root.isFailure, maybe.isFailure);
 		assert.strictEqual(root.createResult, maybe.createResult);
-	});
-});
-
-describe('flow api', () => {
-	it('should contain exposed interfaces', () => {
-		const a:root.processValue<number, string> = value => root.createResult(value.toFixed(0));
-		const b:root.processFailure<unknown, string> = value => value;
-	});
-
-	it('should contain exposed methods', () => {
-		assert.strictEqual(root.may, flow.may);
-		assert.strictEqual(root.and, flow.and);
-		assert.strictEqual(root.or, flow.or);
-		assert.strictEqual(root.resolve, flow.resolve);
+		assert.strictEqual(root.createFailure, maybe.createFailure);
 	});
 });
 
 describe('native api', () => {
 	it('should contain exposed methods', () => {
 		assert.strictEqual(root.maybeFrom, native.maybeFrom);
+		assert.strictEqual(root.maybeAsync, native.maybeAsync);
 		assert.strictEqual(root.resultFrom, native.resultFrom);
+		assert.strictEqual(root.resultAsync, native.resultAsync);
+		assert.strictEqual(root.failureFrom, native.failureFrom);
+		assert.strictEqual(root.failureAsync, native.failureAsync);
+	});
+});
+
+describe('conversion api', () => {
+	it('should contain exposed methods', () => {
+		assert.strictEqual(root.all, convert.all);
+		assert.strictEqual(root.allAsync, convert.allAsync);
+		assert.strictEqual(root.may, convert.may);
+		assert.strictEqual(root.resolve, convert.resolve);
+	});
+});
+
+describe('flow api', () => {
+	it('should contain exposed methods', () => {
+		assert.strictEqual(root.and, flow.and);
+		assert.strictEqual(root.andAsync, flow.andAsync);
+		assert.strictEqual(root.or, flow.or);
+		assert.strictEqual(root.orAsync, flow.orAsync);
+		assert.strictEqual(root.onResult, flow.onResult);
+		assert.strictEqual(root.onResultAsync, flow.onResultAsync);
+		assert.strictEqual(root.onFailure, flow.onFailure);
+		assert.strictEqual(root.onFailureAsync, flow.onFailureAsync);
+	});
+});
+
+describe('composition api', () => {
+	it('should contain exposed methods', () => {
+		assert.strictEqual(root.apply, compose.apply);
 	});
 });
