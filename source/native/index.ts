ad80af68a9b816ae669cde633d1e752bd2eb50d9
ck <ck@chkt.one>
2024-02-25 20:06:14 +0000

    Expose filterAll()

--- source/native/index.ts
+++ source/native/index.ts
@@ -1,2 +1,3 @@
+export { filterAll } from './common';
 export { maybeFrom, resultFrom, failureFrom } from './sync';
 export { maybeFrom as maybeAsync, resultFrom as resultAsync, failureFrom as failureAsync } from './async';
--- test/native/index.ts
+++ test/native/index.ts
@@ -2,11 +2,13 @@ import * as assert from 'node:assert';
 import { describe, it } from 'mocha';
 import * as root from '../../source/native';
 import * as async from '../../source/native/async';
+import * as common from '../../source/native/common';
 import * as sync from '../../source/native/sync';
 
 
 describe('module', () => {
 	it('should expose public functions', () => {
+		assert.strictEqual(root.filterAll, common.filterAll);
 		assert.strictEqual(root.maybeFrom, sync.maybeFrom);
 		assert.strictEqual(root.resultFrom, sync.resultFrom);
 		assert.strictEqual(root.failureFrom, sync.failureFrom);
